8c53e4874bf32839891fb67da546207d5dd3d4d6
emmanuel <odoemmanuel100@gmail.com>
2020-08-09 00:27:33 +0100

    cimplemented  anan update on best_anwser_id on questions table when answer is been removed

--- app/Answer.php
+++ app/Answer.php
@@ -34,7 +34,12 @@ class Answer extends Model
              });
 
          static::deleted(function($answer){
-             $answer->question->decrement('answers_count');
+             $question = $answer->question;
+             $question->decrement('answers_count');
+             if($question->best_answer_id == $answer->id){
+                 $question->best_answer_id = NULL;
+                 $question->save();
+             }
          });
      }
 
@@ -42,6 +47,12 @@ class Answer extends Model
     public function getCreatedDateAttribute(){
 
         return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute()
+    {
+        # code...
+        return $this->id == $this->question->best_answer_id ? 'vote-accepted' : '';
     }
 
 }
--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -19,7 +19,7 @@
                                         <i class="fas fa-caret-down fa-3x"></i>
 
                                     </a>
-                                    <a title="Mark this answer as best answer" class="vote-accepted mt-2">
+                                    <a title="Mark this answer as best answer" class="{{ $answer->status }}">
                                         <i class="fas fa-check fa-2x"></i>
                                     </a>
                                </div>
